dbcb109eaa8c5e38ebde2a656cf193c22fe9f48e
abudaan <daniel@abumarkub.net>
2020-08-02 10:09:59 +0200

    update convert script

--- util/convert.ts
+++ util/convert.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 
+// process.args: string dev or pub, string version heartbeat, string version WebDAW
 const run = async () => {
-  const dev = process.argv[2] === 'true';
+  const dev = process.argv[2] === 'dev';
   // console.log(dev);
   const p = path.join('../', 'package.json');
   const f = await fs.promises.readFile(p, 'utf-8');
@@ -11,8 +12,8 @@ const run = async () => {
     j.dependencies['heartbeat-sequencer'] = '../heartbeat';
     j.dependencies['webdaw-modules'] = '../WebDAW';
   } else {
-    j.dependencies['heartbeat-sequencer'] = '0.0.21';
-    j.dependencies['webdaw-modules'] = '0.0.8';
+    j.dependencies['heartbeat-sequencer'] = process.argv[3];
+    j.dependencies['webdaw-modules'] = process.argv[4];
   }
   await fs.promises.writeFile(p, JSON.stringify(j));
 };
