9396543059e8d9e55ff5547b167d214936e11f84
abudaan <daniel@abumarkub.net>
2020-08-02 14:44:37 +0200

    wip selection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import sequencer, { loadMusicXMLFile, MIDIEvent, Song } from 'heartbeat-sequencer';
 import { OpenSheetMusicDisplay } from 'opensheetmusicdisplay';
+import sequencer, { loadMusicXMLFile, MIDIEvent, Song } from 'heartbeat-sequencer';
 import {
   parseMusicXML,
   setGraphicalNoteColor,
@@ -8,17 +8,33 @@ import {
   MusicSystemShim,
   getVersion,
   NoteMappingMIDIToGraphical,
+  getGraphicalNotesInSelection,
+  NoteMappingGraphicalToMIDI,
+  GraphicalNoteData,
 } from 'webdaw-modules';
 import { loadJSON, addAssetPack, loadMIDIFile } from './heartbeat-utils';
 
 const ppq = 960;
-const midiFile = 'mozk545a_musescore';
+// const midiFile = '../assets/mozk545a_musescore.mid';
+// const mxmlFile = '../assets/mozk545a_musescore.musicxml';
+const midiFileName = 'spring';
+const midiFile = '../assets/spring.mid';
+const mxmlFile = '../assets/spring.xml';
 const instrumentName = 'TP00-PianoStereo';
+const instrumentOgg = `../assets/${instrumentName}.ogg.json`;
+const instrumentMp3 = `../assets/${instrumentName}.mp3.json`;
+let midiToGraphical: NoteMappingMIDIToGraphical;
+let graphicalToMidi: NoteMappingGraphicalToMIDI;
+let graphicalNotesPerBar: GraphicalNoteData[][];
+let song: Song;
+let repeats: number[][];
+let initialTempo: number;
 
 const btnPlay = document.getElementById('play') as HTMLButtonElement;
 const btnStop = document.getElementById('stop') as HTMLButtonElement;
 const scoreDiv = document.getElementById('score');
-if (scoreDiv === null) {
+const selectionDiv = document.getElementById('selection');
+if (scoreDiv === null || selectionDiv === null) {
   throw new Error('element not found');
 }
 
@@ -30,26 +46,23 @@ console.log(`OSMD: ${osmd.Version}`);
 console.log(`WebDAW: ${getVersion()}`);
 
 // reset all highlighted notes
-const resetScore = (midiToGraphical: NoteMappingMIDIToGraphical) => {
+const resetScore = () => {
   Object.values(midiToGraphical).forEach(g => {
     const { element } = g;
     setGraphicalNoteColor(element, 'black');
   });
 };
 
-const resize = async (song: Song, repeats: number[][]) => {
+const resize = async () => {
   osmd.render();
-
   // the score has been rendered so we can get all references to the SVGElement of the notes
-  const graphicalNotesPerBar = await getGraphicalNotesPerBar(osmd, ppq);
-
+  graphicalNotesPerBar = await getGraphicalNotesPerBar(osmd, ppq);
   // map the MIDI notes (MIDINote) to the graphical notes (SVGElement)
-  const { midiToGraphical, graphicalToMidi } = mapMIDINoteIdToGraphicalNote(
+  ({ midiToGraphical, graphicalToMidi } = mapMIDINoteIdToGraphicalNote(
     graphicalNotesPerBar,
     repeats,
     song.notes
-  );
-
+  ));
   // setup listener for highlighting the active notes and for the scroll position
   let scrollPos = 0;
   let currentY = 0;
@@ -59,7 +72,6 @@ const resize = async (song: Song, repeats: number[][]) => {
     if (midiToGraphical[noteId]) {
       const { element, musicSystem } = midiToGraphical[noteId];
       setGraphicalNoteColor(element, 'red');
-
       const tmp = ((musicSystem as unknown) as MusicSystemShim).graphicalMeasures[0][0].stave.y;
       if (currentY !== tmp) {
         currentY = tmp;
@@ -76,7 +88,6 @@ const resize = async (song: Song, repeats: number[][]) => {
       }
     }
   });
-
   // setup listener to switch of the highlighting if notes are not active anymore
   song.addEventListener('event', 'type = NOTE_OFF', (event: MIDIEvent) => {
     const noteId = event.midiNote.id;
@@ -85,7 +96,6 @@ const resize = async (song: Song, repeats: number[][]) => {
       setGraphicalNoteColor(element, 'black');
     }
   });
-
   // setup listeners for every graphical note
   Object.values(midiToGraphical).forEach(({ element }) => {
     element.addEventListener('mousedown', e => {
@@ -94,7 +104,7 @@ const resize = async (song: Song, repeats: number[][]) => {
       const noteOff = midiNote.noteOff as MIDIEvent;
       if (e.ctrlKey) {
         song.setPlayhead('ticks', noteOn.ticks);
-        resetScore(midiToGraphical);
+        resetScore();
         setGraphicalNoteColor(element, 'red');
       } else {
         setGraphicalNoteColor(element, 'red');
@@ -118,36 +128,30 @@ const resize = async (song: Song, repeats: number[][]) => {
 
 const init = async () => {
   await sequencer.ready();
-
   // load MIDI file and setup song
-  await loadMIDIFile(`../assets/${midiFile}.mid`);
-  const song = sequencer.createSong(sequencer.getMidiFile(midiFile));
-
+  await loadMIDIFile(midiFile);
+  song = sequencer.createSong(sequencer.getMidiFile(midiFileName));
   // load instrument and setup all tracks
-  let url = `../assets/${instrumentName}.mp3.json`;
+  let url = instrumentMp3;
   if (sequencer.browser === 'firefox') {
-    url = `../assets/${instrumentName}.ogg.json`;
+    url = instrumentOgg;
   }
   const json = await loadJSON(url);
   await addAssetPack(json);
   song.tracks.forEach(track => {
     track.setInstrument(instrumentName);
   });
-
   // load MusicXML
-  const xmlDoc = await loadMusicXMLFile('../assets/mozk545a_musescore.musicxml');
+  const xmlDoc = await loadMusicXMLFile(mxmlFile);
   await osmd.load(xmlDoc);
-
   // parse the MusicXML file to find where the song repeats
   const parsed = parseMusicXML(xmlDoc, ppq);
   if (parsed === null) {
     return;
   }
-  const { repeats, initialTempo } = parsed;
-
+  ({ repeats, initialTempo } = parsed);
   // the score gets rendered every time the window resizes; here we force the first render
-  await resize(song, repeats);
-
+  await resize();
   // setup controls
   song.addEventListener('stop', () => {
     btnPlay.innerHTML = 'play';
@@ -168,15 +172,47 @@ const init = async () => {
   });
   btnStop.addEventListener('click', () => {
     song.stop();
+    resetScore();
   });
-
   // everything has been setup so we can enable the buttons
   btnPlay.disabled = false;
   btnStop.disabled = false;
 
   window.addEventListener('resize', async () => {
-    await resize(song, repeats);
+    await resize();
   });
+
+  const selectionStartPoint: { x: number; y: number } = { x: -1, y: -1 };
+  const selectionEndPoint: { x: number; y: number } = { x: -1, y: -1 };
+  const drawSelect = (e: MouseEvent) => {
+    selectionDiv.style.left = `${selectionStartPoint.x}px`;
+    selectionDiv.style.top = `${selectionStartPoint.y}px`;
+    selectionDiv.style.width = `${e.clientX - selectionStartPoint.x}px`;
+    selectionDiv.style.height = `${e.clientY - selectionStartPoint.y}px`;
+    selectionEndPoint.x = e.clientX;
+    selectionEndPoint.y = e.clientY;
+  };
+
+  const stopSelect = (e: MouseEvent) => {
+    document.removeEventListener('mouseup', stopSelect);
+    document.removeEventListener('mousemove', drawSelect);
+    selectionDiv.style.display = 'none';
+    selectionDiv.style.left = '0px';
+    selectionDiv.style.top = '0px';
+    selectionDiv.style.width = '0px';
+    selectionDiv.style.height = '0px';
+    getGraphicalNotesInSelection(graphicalNotesPerBar, selectionStartPoint, selectionEndPoint);
+  };
+
+  const startSelect = (e: MouseEvent) => {
+    selectionStartPoint.x = e.clientX;
+    selectionStartPoint.y = e.clientY;
+    selectionDiv.style.display = 'block';
+    document.addEventListener('mouseup', stopSelect);
+    document.addEventListener('mousemove', drawSelect);
+  };
+
+  document.addEventListener('mousedown', startSelect);
 };
 
 init();
